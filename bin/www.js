a5d0bf56372a568c6865a0c6bdefa0a5f5a1c408
郑玺 <dongsun.zx@raycloud.com>
2019-12-06 20:51:56 +0800

    zx-server

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
 
+let program = require('commander');
+let configs = {
+    '-p, --port <val>': 'set http-server port',
+    '-d, --dir <dir>': 'set http-server directory'
+}
+Object.entries(configs).forEach(([key, value]) => {
+    program.option(key, value);
+})
+
+program.name("zx-server").usage('<options>');
+program.on('--help', function() {
+    console.log('Examples:');
+    console.log(`  $ zx-server --port 3000`);
+})
+let params = program.parse(process.argv); // 用户传过来的参数
+
 const Server = require('../static-server');
-
-
-let server = new Server({ port: 4000 });
+let defaultConfig = {
+    port: 4000,
+    dir: process.cwd(),
+    ...params
+}
+let server = new Server(defaultConfig);
 server.start();
--- static-server.js
+++ static-server.js
@@ -2,10 +2,11 @@ const path = require('path');
 const http = require('http');
 const url = require('url');
 const fs = require('fs').promises;
-const { createReadStream } = require('fs');
+const { createReadStream, readFileSync } = require('fs');
 const mime = require('mime');
 const nunjucks = require('nunjucks');
-
+const chalk = require('chalk');
+const crypto = require('crypto');
 class Server {
     constructor(config) {
         this.port = config.port;
@@ -15,7 +16,7 @@ class Server {
         let { pathname } = url.parse(req.url);
         pathname = decodeURIComponent(pathname);
         if(pathname === '/favicon.ico') return res.end(`Not Found`);
-        let absPath = path.join(__dirname, pathname);
+        let absPath = path.join(this.dir, pathname);
         try {
             let statObj = await fs.stat(absPath);
             if (statObj.isFile()) {
@@ -36,7 +37,35 @@ class Server {
             this.sendError(error, res)
         }
     }
+    hasCache(absPath, req, res, statObj) {
+        // 强缓存10毫秒
+        res.setHeader('Cache-Control', 'max-age=10'); 
+        res.setHeader('Expires', new Date(Date.now() + 10*1000).toGMTString());
+        
+        // 协商缓存
+        let ctime = statObj.ctime.toGMTString(); // 拿到文件变化的时间
+        res.setHeader('Last-Modified', ctime);
+        let content = readFileSync(absPath, 'utf8');
+        let etag = crypto.createHash('md5').update(content).digest('base64');
+        res.setHeader('Etag', etag);
+
+        //第二次访问， 取值对比
+        let ifModifiedSince = req.headers['if-modified-since'];
+        if (ifModifiedSince != ctime) {
+            return false;
+        }
+        let ifNoneMatch = req.headers['if-none-match'];
+        if (ifNoneMatch != etag) {
+            return false
+        }
+        return true;
+
+    }
     sendFile(absPath, req, res, statObj) {
+        if(this.hasCache(absPath, req, res, statObj)) {
+            res.statusCode = 304;
+            return res.end();
+        }
         res.setHeader('Content-type', mime.getType(absPath) + ";charset=utf-8");
         createReadStream(absPath).pipe(res);
     }
@@ -47,7 +76,10 @@ class Server {
     start() {
         let server = http.createServer(this.serverHandler.bind(this));
         server.listen(this.port, () => {
-            console.log('启动成功');
+            console.log(`${chalk.yellow('Starting up http-server, serving') }./${this.dir.split('/').pop()}
+    Available on:
+    http://127.0.0.1:${chalk.green(this.port)}
+    Hit CTRL-C to stop the server`);
         })
     }
 }
